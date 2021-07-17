0bb6d508841233856ee59b0c404b8866f80450e2
Alex <alexandru.gatu@gmail.com>
2021-07-17 11:56:09 +0300

    updated with generic getDriver() method

--- src/test/java/TestClasses.java
+++ src/test/java/TestClasses.java
@@ -1,3 +1,4 @@
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
@@ -10,37 +11,65 @@ public class TestClasses {
     public static final String URL_BASE = "https://github.com/";
     public static final String DRIVERS_PATH = "src/test/resources/drivers/" ;
 
+    public WebDriver getDriver(String browser) {
+        WebDriver driver = null;
+        switch (browser) {
+            case "chrome" : {
+                System.setProperty("webdriver.chrome.driver", DRIVERS_PATH + "chromedriver.exe");
+                driver = new ChromeDriver();
+                break;
+            }
+            case "firefox" : {
+                System.setProperty("webdriver.gecko.driver",DRIVERS_PATH + "geckodriver.exe");
+                driver = new FirefoxDriver();
+                break;
+            }
+            case "edge" : {
+                System.setProperty("webdriver.edge.driver", DRIVERS_PATH + "msedgedriver.exe");
+                driver = new EdgeDriver();
+                break;
+            }
+            default: {
+                throw new IllegalArgumentException("The value provided for the browser type is illegal: " + browser);
+            }
+        }
+        return driver;
+    }
+
     @Before
-    public void BeforeTest() {
+    public void beforeTest() {
         System.out.println("This surely runs each time before any test!!");
+
     }
 
     @Test
-    public void Test01() {
+    public void test01() {
         System.out.println("This is my very first test !");
-        System.setProperty("webdriver.chrome.driver", DRIVERS_PATH + "chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = getDriver("chrome");
         driver.navigate().to(URL_BASE);
         driver.close();
     }
 
     @Test
-    public void Test02() {
+    public void test02() {
         System.out.println("Firefox first test");
-        System.setProperty("webdriver.gecko.driver",DRIVERS_PATH + "geckodriver.exe");
-        WebDriver driver = new FirefoxDriver();
+        WebDriver driver = getDriver("firefox");
         driver.navigate().to(URL_BASE);
         driver.close();
     }
 
     @Test
-    public void Test03() {
+    public void test03() {
         System.out.println("Edge first test");
-        System.setProperty("webdriver.edge.driver", DRIVERS_PATH + "msedgedriver.exe");
-        WebDriver driver = new EdgeDriver();
+        WebDriver driver = getDriver("edge");
         driver.navigate().to(URL_BASE);
         driver.close();
     }
 
+
+    @After
+    public void afterTest() {
+        System.out.println("This is the After method that runs after each tests !");
+    }
 
 }
