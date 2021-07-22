9b02cd503de3c0d6e6da34e24a02e08d244262ce
Alex <alexandru.gatu@gmail.com>
2021-07-22 11:06:20 +0300

    Test reporting - extent reports added

--- src/test/java/Utils/ExtentReport/ExtentManager.java
+++ src/test/java/Utils/ExtentReport/ExtentManager.java
@@ -1,4 +1,63 @@
 package Utils.ExtentReport;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.ChartLocation;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import org.openqa.selenium.Platform;
+
+import java.io.File;
+
 public class ExtentManager {
+
+    private static ExtentReports extent;
+    private static Platform platform;
+    private static String reportFilename = "Test_Automation_Report.html";
+    private static String windowsPath = System.getProperty("user.dir") + "\\TestReport";
+    private static String winReportFileLocation =windowsPath + "\\" + reportFilename;
+
+    private static ExtentReports getInstance() {
+        if (extent == null) {
+            return createInstance();
+        }
+        return extent;
+    }
+
+    static ExtentReports createInstance() {
+        String fileName = getReportLocation();
+        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(fileName);
+        htmlReporter.config().setTestViewChartLocation(ChartLocation.BOTTOM);
+        htmlReporter.config().getChartVisibilityOnOpen();
+        htmlReporter.config().setTheme(Theme.STANDARD);
+        htmlReporter.config().setDocumentTitle(fileName);
+        htmlReporter.config().setEncoding("utf-8");
+        htmlReporter.config().setReportName(fileName);
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+        return extent;
+    }
+
+    private static String getReportLocation() {
+        String reportLocation = null;
+        reportLocation = winReportFileLocation;
+        createReportPath(windowsPath);
+        System.out.println("Report location " + windowsPath);
+        return reportLocation;
+    }
+
+    private static void createReportPath(String windowsPath) {
+        File dir = new File (windowsPath);
+        if (!dir.exists()) {
+            if (dir.mkdir()) {
+                System.out.println("Folder was created successfully for the report !");
+            }
+            else {
+                System.out.println("Fail to create folder for report");
+            }
+        }
+        else {
+            System.out.println("The folder already exists on the disk");
+        }
+    }
+
 }
--- src/test/java/Utils/ExtentReport/TestListener.java
+++ src/test/java/Utils/ExtentReport/TestListener.java
@@ -1,4 +1,62 @@
 package Utils.ExtentReport;
 
-public class TestListener {
+import Tests.TestNgTests.BaseClass;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestListener extends BaseClass implements ITestListener {
+
+    private static ExtentReports report = ExtentManager.createInstance();
+    private static  ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        System.out.println("Extent Reports started!");
+        ExtentTest extentTest = report.createTest(result.getMethod().getMethodName(),
+                result.getMethod().getDescription());
+        test.set(extentTest);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        System.out.println(result.getMethod().getMethodName() + " passed !");
+        test.get().pass("Test passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        System.out.println(result.getMethod().getMethodName() + " failed !");
+        test.get().fail(result.getThrowable());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        System.out.println(result.getMethod().getMethodName() + " skipped !");
+        test.get().fail(result.getThrowable());
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+        System.out.println("Test failed but with success percentage for " + result.getMethod().getMethodName());
+    }
+
+    @Override
+    public void onTestFailedWithTimeout(ITestResult result) {
+        System.out.println("Test failed with timeout for " + result.getMethod().getMethodName());
+
+    }
+
+    @Override
+    public void onStart(ITestContext context) {
+        System.out.println("Extent report started!");
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        System.out.println("Extent report finished!");
+        report.flush();
+    }
 }
