29196bae02e33c9d2727af436d35eb0bd72c4c65
Alex <alexandru.gatu@gmail.com>
2021-08-03 10:01:01 +0300

    Cookie tests

---
+++ src/test/java/Tests/TestNgTests/DemoTests/CookieTests.java
@@ -0,0 +1,64 @@
+package Tests.TestNgTests.DemoTests;
+
+import Tests.TestNgTests.BaseClass;
+import Utils.GeneralUtils;
+import org.openqa.selenium.Cookie;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Date;
+import java.util.Set;
+
+import static Utils.ConfigReader.URL;
+
+public class CookieTests extends BaseClass {
+
+    @Test
+    public void cookieTest01(){
+        //driver.get(URL);
+        driver.get("https://magazinulcolectionarului.ro/");
+        GeneralUtils.printCookies(driver);
+
+        // Adding a new Cookie, while on the site
+        Cookie c = new Cookie("alexCookie","12345");
+        // add cookie to driver / browser
+        driver.manage().addCookie(c);
+
+        // Adding a cookie from the start (without being on any site)
+        Cookie c2 = new Cookie(
+                "cookie1",
+                "value1",
+                "magazinulcolectionarului.ro",
+                "/",
+                new Date());
+        driver.manage().addCookie(c2);
+
+        GeneralUtils.printCookies(driver);
+        // Delete a cookie that we created
+        driver.manage().deleteCookie(c);
+        GeneralUtils.printCookies(driver);
+
+        // Delete a cookie based on its name
+        driver.manage().deleteCookieNamed("PHPSESSID");
+        GeneralUtils.printCookies(driver);
+
+        // Delete all cookies
+        driver.manage().deleteAllCookies();
+        GeneralUtils.printCookies(driver);
+    }
+
+    @Test
+    public void cookieTest02(){
+        driver.get("https://magazinulcolectionarului.ro/");
+        Set<Cookie> cookies = driver.manage().getCookies();
+        Cookie phpCookie = driver.manage().getCookieNamed("PHPSESSID");
+
+        Assert.assertEquals(phpCookie.getName(), "PHPSESSID");
+
+        Assert.assertTrue(phpCookie.isSecure());
+
+        Assert.assertTrue(GeneralUtils.checkCookieName(driver, "PHPSESSID"));
+    }
+
+
+}
--- src/test/java/Utils/GeneralUtils.java
+++ src/test/java/Utils/GeneralUtils.java
@@ -1,5 +1,10 @@
 package Utils;
 
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+
+import java.util.Set;
+
 public class GeneralUtils {
 
     public static String stackTraceConvert(Object[] obj) {
@@ -9,6 +14,32 @@ public class GeneralUtils {
             output.append("\n");
         }
         return output.toString();
+    }
+
+    public static void printCookies(WebDriver driver) {
+        Set<Cookie> cookies = driver.manage().getCookies();
+        System.out.println("Number of cookies found: " + cookies.size());
+        for (Cookie c: cookies) {
+            System.out.println("Cookie: " + c.getName());
+            System.out.println("--> Value: " + c.getValue());
+            System.out.println("--> Domain: " + c.getDomain());
+            System.out.println("--> Path: " + c.getPath());
+            System.out.println("--> Expires: " + c.getExpiry());
+            System.out.println();
+
+        }
+    }
+
+    // A method for searching if a cookie with specific name is present in the browser
+    public static boolean checkCookieName(WebDriver driver, String cookieName) {
+        Set<Cookie> cookies = driver.manage().getCookies();
+        boolean found = false;
+        for (Cookie c : cookies) {
+            if (c.getName().toLowerCase().equals(cookieName.toLowerCase())) {
+                found = true;
+            }
+        }
+        return found;
     }
 
 }
