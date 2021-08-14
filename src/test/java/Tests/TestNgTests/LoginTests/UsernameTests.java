4c012c2d4ef3cfea5e420e78b671a03a77f95227
Alex <alexandru.gatu@gmail.com>
2021-08-14 10:59:46 +0300

    dataprovider with 8 tests for username checking the charset and length

--- src/test/java/Tests/TestNgTests/LoginTests/UsernameTests.java
+++ src/test/java/Tests/TestNgTests/LoginTests/UsernameTests.java
@@ -16,7 +16,6 @@ import static Utils.ConfigReader.URL;
 
 public class UsernameTests extends BaseClass {
 
-
     @DataProvider(name = "UsernameDataProvider")
     public Iterator<Object[]> usernameData() {
         Collection<Object[]> dp = new ArrayList<>();
@@ -26,6 +25,8 @@ public class UsernameTests extends BaseClass {
         dp.add(new String[] { "8", "", "1" });
         dp.add(new String[] { "8", "Username does not allow special characters", "4" });
         dp.add(new String[] { "8", "Username does not allow numbers", "3" });
+        dp.add(new String[] { "8", "Username does not allow numbers", "6" });
+        dp.add(new String[] { "0", "Username is required!", "1" });
         return dp.iterator();
     }
 
@@ -34,6 +35,8 @@ public class UsernameTests extends BaseClass {
     //ALT-531:Username with less than minimum length
     //ALT-534:Username with maximum length
     //ALT-539:Username with special chars
+    //ALT-540:Username with alphanumeric chars
+    //ALT-542:Username with null
     // BONUS - username with numbers only
     @Test(dataProvider = "UsernameDataProvider")
     public void testUsernameLength(String lengthString, String errorMessage, String charsetType) {
@@ -46,12 +49,13 @@ public class UsernameTests extends BaseClass {
         WebElement passwordLabel = driver.findElement(By.cssSelector("#login-form > div.form-group.row.row-cols-lg-true > label"));
         Assert.assertEquals(usernameLabel.getText(),"Username");
         Assert.assertEquals(passwordLabel.getText(),"Password");
+
         // Assert that the "Authentication" text is present on the page
         WebElement authBannerText = driver.findElement(By.cssSelector("small.text-muted"));
         Assert.assertEquals(authBannerText.getText(),"Authentication");
 
-        // Insert into the username field the max number of characters allowed
-        // Identify the username field in the page and insert a random string with length 8
+        // Insert into the username field the number of characters for each case
+        // Identify the username field in the page and insert a random string with specific length and charset type
         WebElement usernameInput = driver.findElement(By.name("username"));
         WebElement passwordInput = driver.findElement(By.name("password"));
         usernameInput.clear();
@@ -62,42 +66,9 @@ public class UsernameTests extends BaseClass {
         passwordInput.sendKeys(GeneralUtils.getRandomStringByLength(8, 5));
 
         // If the username is invalid (not registered) there is an error generated. No errors related to the length of the username is displayed.
-        // Grab the error message (if any) from the username field and assert that the message is not related with max length
+        // Grab the error message (if any) from the username field and assert that the message is as expected
         WebElement usernameError = driver.findElement(By.cssSelector("#login-form > div:nth-child(1) > div > div > div.text-left.invalid-feedback"));
         Assert.assertEquals(usernameError.getText(),errorMessage);
-    }
-
-
-    //ALT-540:Username with alphanumeric chars
-    @Test
-    public void usernameAlphanumeric() {
-        // Go to authentication page
-        driver.get(URL + "#/login");
-
-        // Verify that the login page is displayed
-        // Assert that the "Authentication" text is present on the page
-
-        // In the username field insert a random string between 3-8 chars that contains both numerical and alphabetical
-        // Identify the username field, generate a random string with numerical chars and input in the field
-
-        // There is an error related with the fact that numerical chars are not allowed
-        // Grab the error message from the username field and assert that the message is correct
-    }
-
-    // ALT-542:Username with null
-    @Test
-    public void usernameNull(){
-        // Go to authentication page
-        driver.get(URL + "#/login");
-
-        // Verify that the login page is displayed
-        // Assert that the "Authentication" text is present on the page
-
-        // In the username field do not insert any chars
-        // Identify the username field, clear if any chars are present, assert that the length of th input is 0
-
-        // There is an error related with the fact that null input is not allowed
-        // Grab the error message from the username field and assert that the message is correct
     }
 
     //ALT-544:Username with register user
